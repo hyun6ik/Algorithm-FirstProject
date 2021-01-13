8b519f6f4b25d2cd867dc67ba187dca2b3048892
hyun6ik <fkanwm@naver.com>
2021-01-14 06:30:22 +0900

    변수와 출력함수

---
+++ 변수와출력함수.py
@@ -0,0 +1,33 @@
+# a = 1
+# A = 2
+# c = 3
+# print(a, A, c)
+#
+# # 값 교환
+# a, b = 10, 20
+# print(a, b)
+# a, b = b, a
+# print(a, b)
+#
+# # 변수 타입
+# a = 12345
+# print(a)
+# print(type(a))
+# a = 12.123456789123456
+# print(a)
+# print(type(a))
+# a = 'student'
+# print(a)
+# print(type(a))
+
+# 출력 방식
+print("number")
+a, b, c = 1,2,3
+print(a,b,c)
+print("number : ", a,b,c)
+print(a,b,c, sep = ', ')
+print(a,b,c, sep=',')
+print(a,b,c, sep='\n')
+print(a, end ='')
+print(b, end ='')
+print(c)
