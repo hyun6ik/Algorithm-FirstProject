88bb77a80ea9ad900f023ebca06443ac7fe0d20c
hyun6ik <fkanwm@naver.com>
2021-01-15 22:02:03 +0900

    변수입력과 연산자

---
+++ 변수입력과 연산자.py
@@ -0,0 +1,31 @@
+# a = input("숫자를 입력하세요 : ")
+# print(a)
+#
+
+# a, b = input("숫자를 입력하세요 : ").split()
+# print(type(a))
+# c = a+b
+# print(type(c))
+# print(c)
+
+
+# a, b = input("숫자를 입력하세요 : ").split()
+# a = int(a)
+# b = int(b)
+# print(type(a+b))
+# print(a+b)
+
+# a, b = map(int, input("숫자를 입력하세요 : ").split())
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+
+a = 4.3
+b = 5
+c = a + b
+print(type(c))
+print(c)
