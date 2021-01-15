4441bbdb993fdcd625308b1bdf682e828adf54c4
hyun6ik <fkanwm@naver.com>
2021-01-15 22:58:37 +0900

    반복문(for, while, break, continue)

---
+++ 반복문(for, while, break, continue).py
@@ -0,0 +1,40 @@
+# a = range(1, 11)
+# print(list(a))
+
+
+# for i in range(10):
+#     print("Hello {0}".format(i))
+
+
+# for i in range(10, 0, -2):
+#     print(i)
+
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+#
+
+# i = 10
+# while i>=1:
+#     print(i)
+#     i -= 1
+
+# i = 1
+# while True:
+#     print(i)
+#     if i == 10:
+#         break
+#     i += 1
+
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+for i in range(1, 11):
+    print(i)
+    if i > 9:
+        break
+else:
+    print(11)
