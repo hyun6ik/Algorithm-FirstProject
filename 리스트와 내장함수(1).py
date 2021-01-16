6202c5fe43cb6b5a37a9d2f5c773a7efa20f9d37
hyun6ik <fkanwm@naver.com>
2021-01-16 22:58:17 +0900

    리스트와 내장함수(1)

---
+++ 리스트와 내장함수(1).py
@@ -0,0 +1,47 @@
+import random as r
+
+# a = []
+# print(a)
+# b = list()
+# print(b)
+#
+# a = [1,2,3,4,5]
+# print(a)
+# print(a[0])
+#
+# b = list(range(1, 11))
+# print(b)
+#
+# c = a + b
+# print(c)
+
+# a = [1, 2, 3, 4, 5]
+# a.append(6)
+# print(a)
+# a.insert(3, 7)
+# print(a)
+# a.pop()
+# print(a)
+# a.pop(3)
+# print(a)
+#
+# a.remove(4)
+# print(a)
+#
+# print(a.index(5))
+
+a= list(range(1,11))
+print(a)
+print(sum(a))
+print(max(a))
+print(min(a))
+print(min(7, 5))
+print(a)
+r.shuffle(a)
+print(a)
+a.sort()
+print(a)
+a.sort(reverse=True)
+print(a)
+a.clear()
+print(a)
